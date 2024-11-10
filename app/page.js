ed85a1acf4dcbb287eba6680bbc21637718f18b2
philpetersohn <petersohn.phil@gmail.com>
2024-11-09 16:48:50 -0800

    Added daisyui and dependencies

--- app/page.js
+++ app/page.js
@@ -1,7 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-   <h1>Hello Next.js App</h1>
-  );
+  return <button className="btn btn-primary m-2">Click me</button>;
 }
